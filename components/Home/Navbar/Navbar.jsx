e7e4d0e2e97797cbc32a5bb214319498a72ad830
Onur Ardıç <onurardc@outlook.com>
2024-08-01 16:12:48 +0300

    navbar has been added

---
+++ components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,112 @@
+'use client'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import MenuIcon from '@mui/icons-material/Menu'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { useState } from 'react'
+import Link from 'next/link'
+
+const drawerWidth = 240
+
+
+function DrawerAppBar(props) {
+    const { window } = props
+    const [mobileOpen, setMobileOpen] = useState(false)
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState)
+    }
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                MUI
+            </Typography>
+            <Divider />
+            <List sx={{ display: 'flex', flexDirection: "column" }}>
+                <Typography component={Link} href="/home" fontWeight={600} color="purple">
+                    Home
+                </Typography>
+
+                <Typography component={Link} href="/projects" fontWeight={600} color="purple">
+                    Projects
+                </Typography>
+
+                <Typography component={Link} href="/articles" fontWeight={600} color="purple">
+                    Articles
+                </Typography>
+            </List>
+        </Box>
+    )
+
+    const container = window !== undefined ? () => window().document.body : undefined
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar component="nav" sx={{ backgroundColor: "#121212" }}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        MUI
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: '20px' }}>
+                        <ListItem disablePadding>
+                            <Typography component={Link} href="/home" fontWeight={600} color="white">
+                                Home
+                            </Typography>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <Typography component={Link} href="/projects" fontWeight={600} color="white">
+                                Projects
+                            </Typography>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <Typography component={Link} href="/articles" fontWeight={600} color="white">
+                                Articles
+                            </Typography>
+                        </ListItem>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="secondary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    )
+}
+
+export default DrawerAppBar
